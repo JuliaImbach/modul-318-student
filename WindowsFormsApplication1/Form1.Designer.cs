61e0229d7e0fcf724effcdd65672cb9e0d548ea3
dok405 <dok405@DOK405>
2016-05-24 16:27:44 +0200

    Autocomplete hinzufügen

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApplication1
+﻿namespace SwissTransportTimetable
 {
     partial class Form1
     {
@@ -28,44 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.cmdShowHelloMessage = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.cmdSearch = new System.Windows.Forms.Button();
+            this.txtStartStation = new System.Windows.Forms.TextBox();
+            this.lstBoxConnections = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEndStation = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // cmdShowHelloMessage
+            // cmdSearch
             // 
-            this.cmdShowHelloMessage.Location = new System.Drawing.Point(374, 13);
-            this.cmdShowHelloMessage.Name = "cmdShowHelloMessage";
-            this.cmdShowHelloMessage.Size = new System.Drawing.Size(75, 23);
-            this.cmdShowHelloMessage.TabIndex = 0;
-            this.cmdShowHelloMessage.Text = "Hallo";
-            this.cmdShowHelloMessage.UseVisualStyleBackColor = true;
-            this.cmdShowHelloMessage.Click += new System.EventHandler(this.cmdShowHelloMessage_Click);
+            this.cmdSearch.Location = new System.Drawing.Point(372, 71);
+            this.cmdSearch.Name = "cmdSearch";
+            this.cmdSearch.Size = new System.Drawing.Size(75, 23);
+            this.cmdSearch.TabIndex = 0;
+            this.cmdSearch.Text = "Suchen";
+            this.cmdSearch.UseVisualStyleBackColor = true;
+            this.cmdSearch.Click += new System.EventHandler(this.cmdSearch_Click);
             // 
-            // textBox1
+            // txtStartStation
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(335, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtStartStation.Location = new System.Drawing.Point(13, 30);
+            this.txtStartStation.Name = "txtStartStation";
+            this.txtStartStation.Size = new System.Drawing.Size(335, 20);
+            this.txtStartStation.TabIndex = 1;
+            this.txtStartStation.KeyUp += new System.Windows.Forms.KeyEventHandler(this.StartStation_KeyUp);
             // 
-            // listBox1
+            // lstBoxConnections
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(13, 40);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(335, 95);
-            this.listBox1.TabIndex = 2;
+            this.lstBoxConnections.FormattingEnabled = true;
+            this.lstBoxConnections.Location = new System.Drawing.Point(12, 137);
+            this.lstBoxConnections.Name = "lstBoxConnections";
+            this.lstBoxConnections.Size = new System.Drawing.Size(335, 95);
+            this.lstBoxConnections.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Startstation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Endstation";
+            // 
+            // txtEndStation
+            // 
+            this.txtEndStation.Location = new System.Drawing.Point(13, 74);
+            this.txtEndStation.Name = "txtEndStation";
+            this.txtEndStation.Size = new System.Drawing.Size(335, 20);
+            this.txtEndStation.TabIndex = 4;
+            this.txtEndStation.KeyUp += new System.Windows.Forms.KeyEventHandler(this.EndStation_KeyUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Verbindungen";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 147);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.cmdShowHelloMessage);
+            this.ClientSize = new System.Drawing.Size(484, 274);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEndStation);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstBoxConnections);
+            this.Controls.Add(this.txtStartStation);
+            this.Controls.Add(this.cmdSearch);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -75,9 +119,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button cmdShowHelloMessage;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button cmdSearch;
+        private System.Windows.Forms.TextBox txtStartStation;
+        private System.Windows.Forms.ListBox lstBoxConnections;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEndStation;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication1
+namespace SwissTransportTimetable
 {
     public partial class Form1 : Form
     {
@@ -18,13 +18,71 @@ namespace WindowsFormsApplication1
             InitializeComponent();
         }
 
-        private void cmdShowHelloMessage_Click(object sender, EventArgs e)
+        private void cmdSearch_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hallo");
+            string message = "Station ungültig";
 
+            //Stationen auslesen und validieren
+            string startStation = txtStartStation.Text;
+            txtStartStation.Text = SearchStation(startStation) == startStation ? startStation : message;
+
+            string endStation = txtEndStation.Text;
+            txtEndStation.Text = SearchStation(endStation) == endStation ? endStation : message;
+        }
+
+        private void StartStation_KeyUp(object sender, KeyEventArgs e)
+        {
+            //Autocomplete hinzufügen
+            string startStation = txtStartStation.Text;
+            if (e.KeyCode.ToString() != "Back" && e.KeyCode.ToString() != "Delete" && startStation.Length == 3) //Nur bei Textlänge 3, sonst Überlastung
+            {
+
+                txtStartStation.AutoCompleteCustomSource = Autocomplete(startStation);
+                txtStartStation.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+                txtStartStation.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            }
+        }
+
+        private void EndStation_KeyUp(object sender, KeyEventArgs e)
+        {
+            //Autocomplete hinzufügen
+            string endStation = txtEndStation.Text;
+            if (endStation.Length == 3) //Nur bei Textlänge 3, sonst Überlastung
+            {             
+                txtEndStation.AutoCompleteCustomSource = Autocomplete(endStation);
+                txtEndStation.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+                txtEndStation.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            }
+        }
+
+        private string SearchStation(string station)
+        {
+            //Station auslesen
+            Transport transport = new Transport();
+            return transport.GetStations(station) != null ? transport.GetStations(station).StationList[0].Name.ToString() : null;
+            
+        }
+
+        public AutoCompleteStringCollection Autocomplete(string station)
+        {
+            //Liste mit allen Station in eine Liste laden
+            var source = new AutoCompleteStringCollection();
             Transport transport = new Transport();
 
-            MessageBox.Show(transport.ToString());
+            var transportList = transport.GetStations(station).StationList;
+            foreach (var transportStation in transportList)
+            {
+                source.Add(transportStation.Name.ToString());
+            }
+
+            return source;
+        }
+
+        private List<Connection> SearchConnection(string fromStation, string toStation)
+        {
+            //Verbindungen auslesen
+            Transport transport = new Transport();
+            return transport.GetConnections(fromStation, toStation).ConnectionList;
         }
     }
 }
